79cb9ef881139f487317e6e9a634f6dd0cb82946
Thahee02 <developerthahee@gmail.com>
2026-03-15 15:37:12 +0530

    Added detailed version

--- Hashmaps/valid_anagram.py
+++ Hashmaps/valid_anagram.py
@@ -3,6 +3,28 @@ class Solution(object):
         return sorted(s) == sorted(t)
     
 
+# class Solution(object):
+#     def isAnagram(self, s, t):
+#         if len(s) != len(t):
+#             return False
+        
+#         count = {}
+
+#         for char in s:
+#             count[char] = count.get(char, 0) + 1
+        
+#         for char in t:
+#             if char not in count:
+#                 return False
+#             count[char] -= 1
+        
+#         for value in count.values():
+#             if value != 0:
+#                 return False
+        
+#         return True
+
+
 # Example usage:
 solution = Solution()
 print(solution.isAnagram("anagram", "nagaram"))  # Output: True
