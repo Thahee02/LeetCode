121569c8e2054d7eea8ef28f251da5ebc1c5f3cf
Thahee02 <developerthahee@gmail.com>
2026-03-16 16:55:05 +0530

    Solved: 232. Implement Queue using Stacks

--- Stacks and Queues/implement_queue_using_stacks.py
+++ Stacks and Queues/implement_queue_using_stacks.py
@@ -1,25 +1,34 @@
 class MyQueue(object):
 
     def __init__(self):
+        # Initialize your data structure here.
+        # Add 2 stacks, one for input and one for output
         self.stack_in = []
         self.stack_out = []
 
     def push(self, x):
+        # Push element x to the back of queue.
         self.stack_in.append(x)
 
     def pop(self):
+        # Remove the element from the front of the queue and return it.
+        # If the output stack is empty, we need to transfer all elements from the input stack to the output stack
         if not self.stack_out:
             while self.stack_in:
                 self.stack_out.append(self.stack_in.pop())
         return self.stack_out.pop()
     
     def peek(self):
+        # Get the front element.
+        # If the output stack is empty, we need to transfer all elements from the input stack to the output stack
         if not self.stack_out:
             while self.stack_in:
                 self.stack_out.append(self.stack_in.pop())
         return self.stack_out[-1]
     
     def empty(self):
+        # Returns whether the queue is empty.
+        # The queue is empty if both stacks are empty.
         return not self.stack_in and not self.stack_out
         
 
